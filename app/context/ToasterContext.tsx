ed4bbeb3fd399ef82b3e65ec53ab0d2d14e59d6f
Octavio Acevedo Sanchez <octavio.acevedo.sanchez@gmail.com>
2023-11-04 16:21:07 -0500

    Added Environment setup, Auth setup, Auth UI, MongoDB and Prisma setup, NextAuth setup, Register functionality

---
+++ app/context/ToasterContext.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+import { Toaster } from 'react-hot-toast';
+
+const ToasterContext = (): JSX.Element => {
+	return <Toaster />;
+};
+
+export default ToasterContext;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
+import tailwindcssForms from '@tailwindcss/forms';
 
 const config: Config = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		extend: {}
+	},
+	plugins: [
+		// require("@tailwindcss/forms")({
+		// 	strategy: 'class'
+		// })
+		tailwindcssForms({
+			strategy: 'class'
+		})
+	]
+};
+export default config;
